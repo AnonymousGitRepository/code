1f0155981957777f3b4a266f61bd5d48bd65d29e
AnonymousGitRepository <code4nlp2025@gmail.com>
2024-09-13 23:50:08 +0900

    Update alpaca_dataset.py

--- ft_datasets/alpaca_dataset.py
+++ ft_datasets/alpaca_dataset.py
@@ -1,8 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
-# For dataset details visit: https://crfm.stanford.edu/2023/03/13/alpaca.html
-
 import copy
 import json
 import os
